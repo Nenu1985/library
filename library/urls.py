26db8f5f5dc65bc4a9fa3074f5287278eb84c2fa
Nenu1985 <48708587+Nenu1985@users.noreply.github.com>
2019-07-03 11:43:39 +0300

    created book form

--- library/urls.py
+++ library/urls.py
@@ -5,8 +5,8 @@ app_name = 'library'
 
 urlpatterns = [
     path('', views.UserListView.as_view(), name='users-list'),
-    path('books/', views.BookListView.as_view(), name='books-list'),
-    path('books/<int:id>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('books/', views.BookCreateView.as_view(), name='books-list'),
+    path('books/<int:id>/<slug:slug>/', views.BookDetailView.as_view(), name='book-detail'),
 
 
 ]
--- library/views.py
+++ library/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, HttpResponseRedirect
+from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from library.models import Book
+from django.urls import reverse_lazy
+from library.forms import BookCreateForm
+from django.views.decorators.csrf import csrf_protect
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 
@@ -19,3 +24,27 @@ class BookListView(ListView):
 
 class BookDetailView(DetailView):
     model = Book
+
+
+class BookCreateView(CreateView):
+    model = Book
+    template_name = 'library/create_book.html'
+    # success_url = reverse_lazy('library:users-list')
+    form_class = BookCreateForm
+
+    def get_context_data(self, **kwargs):
+        kwargs['books'] = Book.objects.order_by('created')
+        return super(BookCreateView, self).get_context_data(**kwargs)
+
+    @method_decorator(csrf_protect, login_required)
+    def post(self, request, *args, **kwargs):
+
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.user = self.request.user
+            new_item.save()
+            return HttpResponseRedirect(reverse_lazy('library:books-list'))
+        return super(BookCreateView, self).post(request, *args, **kwargs)
+
+
